0aae094527609ff7862b768b91bc482ccd6e633c
leciej <mateusz.leciejewski@gmail.com>
2025-12-15 14:30:45 +0100

    Dodanie DbContextu z tabelami i migracja

--- SolutionOrdersReact/SolutionOrdersReact.Server/Program.cs
+++ SolutionOrdersReact/SolutionOrdersReact.Server/Program.cs
@@ -1,4 +1,8 @@
 
+using Microsoft.EntityFrameworkCore;
+using SolutionOrdersReact.Server.Data;
+using System.Reflection;
+
 namespace SolutionOrdersReact.Server
 {
     public class Program
@@ -11,10 +15,42 @@ namespace SolutionOrdersReact.Server
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            // DbContext
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            // MediatR
+            builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+            // CORS
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowReactApp",
+                    policy => policy
+                        .WithOrigins("https://localhost:5173") // Port domyœlny Vite dla React
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials());
+            });
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
             var app = builder.Build();
+
+            // Automatyczne zastosowanie migracji przy starcie
+            using (var scope = app.Services.CreateScope())
+            {
+                try
+                {
+                    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                    dbContext.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "B³¹d podczas migracji bazy danych");
+                }
+            }
+
 
             app.UseDefaultFiles();
             app.UseStaticFiles();
@@ -27,12 +63,9 @@ namespace SolutionOrdersReact.Server
             }
 
             app.UseHttpsRedirection();
-
+            app.UseCors("AllowReactApp");
             app.UseAuthorization();
-
-
             app.MapControllers();
-
             app.MapFallbackToFile("/index.html");
 
             app.Run();
