eb53e28af11009c9a590a7e6249299811410ceff
leciej <mateusz.leciejewski@gmail.com>
2025-12-15 21:05:37 +0100

    ClientController

---
+++ SolutionOrdersReact/SolutionOrdersReact.Server/Controllers/ClientController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SolutionOrdersReact.Server.Data;
+using SolutionOrdersReact.Server.Models;
+
+namespace SolutionOrdersReact.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController(ApplicationDbContext context) : ControllerBase
+    {
+        // GET: api/Client
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Client>>> GetClients()
+        {
+            return await context.Clients.ToListAsync();
+        }
+
+        // GET: api/Client/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Client>> GetClient(int id)
+        {
+            var client = await context.Clients.FindAsync(id);
+
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return client;
+        }
+
+        // PUT: api/Client/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutClient(int id, Client client)
+        {
+            if (id != client.IdClient)
+            {
+                return BadRequest();
+            }
+
+            context.Entry(client).State = EntityState.Modified;
+
+            try
+            {
+                await context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClientExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Client
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Client>> PostClient(Client client)
+        {
+            context.Clients.Add(client);
+            await context.SaveChangesAsync();
+
+            return CreatedAtAction("GetClient", new { id = client.IdClient }, client);
+        }
+
+        // DELETE: api/Client/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteClient(int id)
+        {
+            var client = await context.Clients.FindAsync(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            context.Clients.Remove(client);
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ClientExists(int id)
+        {
+            return context.Clients.Any(e => e.IdClient == id);
+        }
+    }
+}
